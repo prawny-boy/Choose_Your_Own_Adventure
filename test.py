e4f6decb2bfa4ee3ba1d2deb5e84b28005ec6bda
SoulOfTheAssassin <oli.liu798@gmail.com>
2024-08-15 17:39:22 +0800

    Hello Fatty

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-from keyboard import is_pressed
-from time import sleep
-from termcolor import cprint
-import sys
-def slowprint(str:str, speed:float, attr:list, c='white', wait=3, skip=False) -> None:
-    start = 0
-    for char in str:
-        cprint(char, end='', attrs=attr, color=c)
-        sys.stdout.flush()
-        sleep(speed)
-        if is_pressed("enter") and start > wait and skip:
-            print(str[start:-1])
-            break
-        start += 1
-    sleep(speed)
-    print()
-slowprint("Fattydfghjhgfdsdfghjhgfdsdfgh", 0.01, ["bold"], skip=True)
+#hello fatty
