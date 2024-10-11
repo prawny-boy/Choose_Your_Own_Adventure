95d5020aa2bc2a83bc3ad3cf486e73ca638e55c2
Sean Chan <sean.chan3@student.education.wa.edu.au>
2024-10-11 10:42:34 +0800

    implemented auto install modules into CYOA3

--- CYOA3/CYOA3.py
+++ CYOA3/CYOA3.py
@@ -1,3 +1,15 @@
+from moduleInstall import installIfNeeded
+from datetime import datetime
+def log(message):
+    print(datetime.now().strftime("%a %b %d %H:%M:%S") + " - " + str(message))
+installIfNeeded("termcolor", "termcolor", log = log)
+installIfNeeded("keyboard", "keyboard", log = log)
+installIfNeeded("pygame", "pygame", log = log)
+import sys
+sys.dont_write_bytecode = True # makes sure _pycache_ is not created
+from termcolor import cprint, colored
+from keyboard import is_pressed
+import pygame
 import base64
 
 def b64_encode(s:str) -> str:
@@ -12,6 +24,5 @@ def b64_decode(s:str) -> str:
     sample_string = sample_string_bytes.decode("ascii")
     return sample_string
     
-
 print(b64_encode(input('Encode: ')))
 print(b64_decode(input('Decode: ')))
